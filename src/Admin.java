44082cbb23afb4bd9560ce3aa4037834c1ee71b5
pablo <pablo.villani@ing.austral.edu.ar>
2020-10-07 16:46:35 -0300

    sasasaa

---
+++ src/Admin.java
@@ -0,0 +1,10 @@
+public class Admin  {
+    String usuario;
+    String contraseña;
+
+    public Admin(String usuario,String contraseña){
+        this.contraseña = contraseña;
+        this.usuario = usuario;
+    }
+
+}
---
+++ src/Ciudadano.java
@@ -0,0 +1,10 @@
+public class Ciudadano {
+    String cuil;
+    String celular;
+
+    public Ciudadano(String cuil,String celular){
+          this.cuil = cuil;
+          this.celular = celular;
+    }
+
+}
