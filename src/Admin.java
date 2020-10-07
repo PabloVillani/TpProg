c0e24cc6d3a95110724c05a7dda8d09d55d2197d
Santiago <santiago.gomez@ing.austral.edu.ar>
2020-10-07 18:17:42 -0300

    Ciudadano y Admin actualizados

--- src/Admin.java
+++ src/Admin.java
@@ -1,11 +1,14 @@
-
 public class Admin  {
     String usuario;
     String contraseña;
 
-    public Admin(String usuario,String contraseña){
+    public Admin(String usuario, String contraseña){
         this.contraseña = contraseña;
         this.usuario = usuario;
+    }
+
+    public void desbloquear(Ciudadano c) {
+
     }
 
 }
--- src/Ciudadano.java
+++ src/Ciudadano.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+
 public class Ciudadano {
+    String contraseña;
     String cuil;
     String celular;
+    ArrayList<String> sintomas;
+    int solicitudesRechazadas;
+    boolean bloqueado;
 
-    public Ciudadano(String cuil,String celular){
-          this.cuil = cuil;
-          this.celular = celular;
+    public Ciudadano(String contraseña, String cuil, String celular, ArrayList<String> sintomas, int solicitudesRechazadas, boolean bloqueado){
+        this.contraseña = contraseña;
+        this.cuil = cuil;
+        this.celular = celular;
+        this.sintomas = sintomas;
+        this.solicitudesRechazadas = solicitudesRechazadas;
+        this.bloqueado = bloqueado;
     }
+    public void registrarContacto() {
+        
+    }
+    public void reportarSintomas() {
 
+    }
+    public void darDeBajaSintomas() {
+
+    }
+    public void confirmacionDeEncuentro() {
+
+    }
 }
