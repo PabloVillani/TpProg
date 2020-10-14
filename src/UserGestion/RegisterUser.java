4f6bce220b63f9f53e857f4f045f5c1b158ec502
pablo <pablo.villani@ing.austral.edu.ar>
2020-10-14 16:52:56 -0300

    Traduccion

--- src/UserGestion/RegisterUser.java
+++ src/UserGestion/RegisterUser.java
@@ -3,13 +3,13 @@ import Validators.PasswordValidator;
 import Util.Scanner;
 
 public class RegisterUser {
-    public static void registrarUsuario() { //Si no lo encontro en la base de datos de TraceIT, le pedira una contraseña.
+    public static void RegisterUser() { //Si no lo encontro en la base de datos de TraceIT, le pedira una contraseña.
         String contra = Scanner.getString("Ingrese una contraseña: ");
         if(PasswordValidator.contraValidador(contra)){ //Verifica que la contraseña cumple con los requisitos.
             //ESCRIBIR EN EL USUARIO.TXT
         }
         else{
-            registrarUsuario(); //La contraseña no cumple con los requisitos, entonces repite el proceso.
+            RegisterUser(); //La contraseña no cumple con los requisitos, entonces repite el proceso.
         }
 
     }
