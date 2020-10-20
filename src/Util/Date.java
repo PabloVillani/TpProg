cfe9f8fdb65e3336bc119a654014aa0fee43fdd0
Santiago GR <santiago.gomez@ing.austral.edu.ar>
2020-10-20 17:47:43 -0300

    Date update

--- src/Util/Date.java
+++ src/Util/Date.java
@@ -1,12 +1,18 @@
 package Util;
 //Metodo sacado de la Practica1 de Prog2.
 public class Date {
+        int hour;
         int day;
         int month;
-        public Date(int day, int month) {
+        public Date(int hour, int day, int month) {
+            this.hour = hour;
             this.day = day;
             this.month = month;
         }
+        public int getHour() { return hour; }
+
+        public void setHour() { this.hour = hour;}
+
         public int getDia() {
             return day;
         }
