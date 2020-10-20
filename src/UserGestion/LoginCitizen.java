b5f46c8e7d52c3bfade5738c52a176911be93942
Guido <guidompr@gmail.com>
2020-10-20 17:25:45 -0300

    Added: Fix

--- src/UserGestion/LoginCitizen.java
+++ src/UserGestion/LoginCitizen.java
@@ -13,11 +13,11 @@ import java.util.ArrayList;
 public class LoginCitizen {
     public static void loginUser() {
         String cuil = Scanner.getString("CUIL: ");
-        if(!CUILValidator.cuilValidator(cuil)){ //Valida que el CUIL cumpla con los requisitos
+        if (!CUILValidator.cuilValidator(cuil)) { //Valida que el CUIL cumpla con los requisitos
             loginUser(); //Recursion del inicio de sesion.
         }
         String mobile = Scanner.getString("Celular: ");
-        if(!MobileValidator.mobileValidator(mobile)){ //Valida que el celular cumpla con los requisitos.
+        if (!MobileValidator.mobileValidator(mobile)) { //Valida que el celular cumpla con los requisitos.
             loginUser(); //Recursion del inicio de sesion
         }
         String ubication = UbicationChooser.ubicationChooser();
@@ -27,8 +27,7 @@ public class LoginCitizen {
             if (Finder.tripleValueFinder(cuil, mobile, ubication, users)) { //Busca el CUIL y el celular en la base de datos de TraceIT.
                 String password = Scanner.getString("Contraseña: "); //Si existe, le pide la contraseña.
                 userPassword(cuil, mobile, password, ubication, users); //Verifica la contraseña.
-            }
-            else {
+            } else {
                 System.out.println("No esta registrado."); //Si no lo encuentra, le pide que se registre.
                 RegisterCitizen.RegisterUser(cuil, mobile, ubication);
             }
@@ -40,18 +39,17 @@ public class LoginCitizen {
             loginUser();
         }
     }
-        public static boolean userPassword(String cuil, String mobile, String password, String ubication, ArrayList<String[]> users) {
-            boolean passStart; //Crea un booleano, y busca la contraseña con el Finder.fourValueFinder.
-            if (Finder.fourValueFinder(cuil, mobile, password, ubication, users)) {
-                passStart = true; //Si lo encuentra, devuelve true y termina el proceso de PasswordUsuario.
-            } else {
-                passStart = false;
-                System.out.println("Contraseña incorrecta.");
-                String secondPassword = Scanner.getString("Contraseña: "); //No lo encuentra, pide un nuevo string.
-                userPassword(cuil,mobile,secondPassword, ubication, users); //Recursion con el nuevo String.
-            }
-            return passStart; //Devuelve el booleano
-        }
-    }
 
+    public static boolean userPassword(String cuil, String mobile, String password, String ubication, ArrayList<String[]> users) {
+        boolean passStart; //Crea un booleano, y busca la contraseña con el Finder.fourValueFinder.
+        if (Finder.fourValueFinder(cuil, mobile, password, ubication, users)) {
+            passStart = true; //Si lo encuentra, devuelve true y termina el proceso de PasswordUsuario.
+        } else {
+            passStart = false;
+            System.out.println("Contraseña incorrecta.");
+            String secondPassword = Scanner.getString("Contraseña: "); //No lo encuentra, pide un nuevo string.
+            userPassword(cuil, mobile, secondPassword, ubication, users); //Recursion con el nuevo String.
+        }
+        return passStart; //Devuelve el booleano
+    }
 }
