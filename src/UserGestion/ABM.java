25887edd8ecbe64d4dbb0c63758f4c2f78cbe367
Guido <guidompr@gmail.com>
2020-10-20 19:57:27 -0300

    Modified: ABM

--- src/UserGestion/ABM.java
+++ src/UserGestion/ABM.java
@@ -20,7 +20,7 @@ public class ABM {
     }
 
     public static void remove(Admin admin) throws IOException {
-        admins.remove(admin);
+        admins.remove(admin);//
     }
         public void mod (String user, String password){
 
