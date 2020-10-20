577c5a2d41acb43f83902c4b4d7a0dc06dac760f
Guido <guidompr@gmail.com>
2020-10-20 19:56:23 -0300

    Modified: ABM

--- src/UserGestion/ABM.java
+++ src/UserGestion/ABM.java
@@ -1,19 +1,29 @@
 package UserGestion;
 
 import Admin.Admin;
-import Util.ArrayMaker;
+import Util.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ABM {
-    ArrayList<String[]> admins = ArrayMaker.tripleStringMaker("src/DataBase/ModificableBases/Admins.txt");
-    public void add(String user, String password){
-
+    static ArrayList<Admin> admins;
+    ArrayList<String[]> stringedAdmins = ArrayMaker.doubleStringMaker("src/DataBase/ModificableBases/Admins.txt");
+    public ABM(){
+        this.admins = new ArrayList<Admin>();
     }
-    public void remove(String user){
 
+    public void add(String user, String password) throws IOException{
+        Admin admin = new Admin(user,password);
+        admins.add(admin);
+        Writer.twoValueWriter(user, password, "src/DataBase/ModificableBases/Admins.txt");
     }
-    public void mod(String user, String password){
 
+    public static void remove(Admin admin) throws IOException {
+        admins.remove(admin);
     }
-}
+        public void mod (String user, String password){
+
+        }
+    }
+
