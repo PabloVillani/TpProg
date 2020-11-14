fa97a2adf5354dae8f5f05a158d35043c1a18a9e
FrancoRedivo <franco.redivo@ing.austral.edu.ar>
2020-11-14 19:31:23 -0300

    tests

---
+++ Test/AdminTest.java
@@ -0,0 +1,34 @@
+import Admin.Admin;
+import Events.Symptom;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+
+public class AdminTest {
+
+    @Test
+    public void test01_TheRemovedSymptomShouldBeEliminated(){
+        Admin admin = new Admin();
+
+        admin.deactivateSymptom("Cansancio");
+
+    }
+
+    @Test
+    public void test02_IfSomeoneTriesToRemoveASymptomTheyDontHaveAnExceptionHasToBeThrown(){
+
+    }
+
+    @Test
+    public void test03_WhenASymptomIsActivatedTheUsersSymptomsShouldNotBeNull(){
+
+    }
+
+    @Test
+    public void test04_WhenModifyingASymptomsNameTheNewNameAndTheOldOneShouldBeDiferent(){
+
+    }
+
+
+}
---
+++ Test/PossibleDiseasesTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class PossibleDiseasesTest {
+
+    @Test
+    public void test01_PossibleDiseases(){
+
+    }
+}
